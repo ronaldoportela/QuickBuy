6148ad4dc5002cdc409e3c025600170b3de55129
Ronaldo Francelino Portela <ronaldoportela@gmail.com>
2019-12-01 17:48:14 -0300

    Início implemetação padrão repositório

---
+++ QuickBuy.Repositorio/Repositorios/BaseRespositorio.cs
@@ -0,0 +1,49 @@
+﻿using QuickBuy.Dominio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class BaseRespositorio<TEntity> : IBaseRepositorio<TEntity> where TEntity : class
+    {
+
+        public BaseRespositorio()
+        {
+
+        }
+
+        public void Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public TEntity ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+ }
